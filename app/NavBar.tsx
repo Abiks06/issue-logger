b451274b985429b9084df365f4445d42ccb40cd8
Ayomide_Abikoye <157048243+Abiks06@users.noreply.github.com>
2026-07-16 08:55:01 +0000

    feat: implement initial issue tracking structure with Prisma schema and NavBar navigation

---
+++ app/NavBar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import { IoMdBug } from "react-icons/io";
+import { MdOutlineDashboard } from "react-icons/md";
+import { FaTools } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/", icon: <MdOutlineDashboard /> },
+    { label: "Issues", href: "/issues", icon: <IoMdBug /> },
+  ];
+  const currentPathname = usePathname();
+  console.log(currentPathname);
+
+  return (
+    <nav className="flex items-center justify-between bg-gray-700 border-b-2 border-black p-5">
+      <div className="flex items-center p-5 text-gray-300">
+        <Link href="/">
+          <FaTools />
+        </Link>
+      </div>
+      <ul className="flex space-x-30 items-center pr-15">
+        {links.map((link) => (
+          <li key={link.href} className="flex items-center p-5">
+            <Link
+              href={link.href}
+              className={classNames({
+                "text-red-300": link.href === currentPathname,
+                "text-gray-300": link.href !== currentPathname,
+                "hover:text-zinc-100 hover:font-bold transition-colors flex items-center": true,
+              })}
+            >
+              {link.icon}
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
